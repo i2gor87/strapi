6394a51e06cf263b0e2e848b3ca850c3319a5f39
Igor Khvan <i2gor87@gmail.com>
2024-09-29 11:42:02 +0900

    cors2

--- src/config/middleware.js
+++ src/config/middleware.js
@@ -3,6 +3,9 @@ module.exports = {
     cors: {
       enabled: true,
       origin: ['*'], // Or you can specify a specific domain ['https://your-frontend.com']
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true, // This needs to be true for handling credentials
     },
   },
 };
